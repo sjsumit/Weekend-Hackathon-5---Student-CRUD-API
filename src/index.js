6ebfd2e59c0329fcac0516bad809996fc83ce077
sjsumit <46019235+sjsumit@users.noreply.github.com>
2020-11-16 03:33:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require("body-parser");
 const studentArray = require('./initialData.js');
-const Joi = require("joi");
+const Joi = require('joi');
 const port = 8080
 app.use(express.urlencoded());
 
