0c379b0e813df4d42fd308b78dab28cd6be13fe2
sjsumit <46019235+sjsumit@users.noreply.github.com>
2020-11-16 03:16:24 +0530

    manualChange2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const bodyParser = require("body-parser");
-const studentArray = require('./InitialData.js');
+const studentArray = require('./initialData.js');
 const Joi = require("joi");
 const port = 8080
 app.use(express.urlencoded());
